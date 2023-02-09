4e460d97bdd2d8ff42d807f6bb764bcc12674717
Yutung <cheng056.ece05g@nctu.edu.tw>
2023-02-09 17:08:15 +0800

    test=hello first self-written test program

---
+++ cv-nanhu/tests/programs/custom/hello/hello.c
@@ -0,0 +1,16 @@
+//
+// This is a very simple test program to demostrate \
+// work flow of user-program verification by cv-yunleng
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	
+    printf(" hello world yunleng, 2023 \n");
+    
+    return EXIT_SUCCESS;
+
+}
