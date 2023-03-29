5156e275f2c0432c255cd94fb5935b30c9ef0d98
Yutung <cheng056.ece05g@nctu.edu.tw>
2023-03-29 14:37:24 +0800

    add flags to indicate usage; flag -p for prog folder; flag -h for help

--- cv32e40p/sim/core/run_coretb.sh
+++ cv32e40p/sim/core/run_coretb.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-echo "Welcome to P200 core-tb run flow"
-echo "user program as $1"
+while getopts "hp:cs?" opt; do
+	case $opt in
+		h)
+			echo "usage of P200 core-tb execute and simulation script:"
+			echo "-h help"
+			echo "-p user program (dir)"
+			echo "-c core/platform name"
+			echo "-s simulation tool"
+			;;
+		p)
+			echo "user program dir: "
+			USER_PROG_DIR=$OPTARG
+			;;
+		?)
+			echo "usage: run_coretb.sh -h"
+			;;
+	esac
+done
 
-if [ -z $1 ]
+if [ -z $USER_PROG_DIR ]
 then
-	echo "Error: empty input prog"
-	echo "Usag as ./run_coretb <prog folder>"
+	echo "Error: must input user program dir"
+	echo "Usag as ./run_coretb -p <user program dir>"
 	exit 0
 fi
 
-# Variables used in the makefile
-export USER_PROG_DIR=$1
-export COMMON_MK=$CORE_V_VERIF/mk/Common.mk
-export SIM_RESULT_DIR=$1_sim_result
-export VERI_OBJ_DIR=$SIM_RESULT_DIR/cobj_dir
+echo "==information=="
+echo "user program dir: $USER_PROG_DIR"
+echo "core/platform: cv32e40p"
+echo "simulation tool: vcs"
+echo "execute after build: yes"
+echo "==information=="
 
+# Variables used in the makefile
+export COMMON_MK=$CORE_V_VERIF/mk/Common.mk
+export SIM_RESULT_DIR=${USER_PROG_DIR}_sim_result
+export VERI_OBJ_DIR=${SIM_RESULT_DIR}/cobj_dir
 
 # Build core-tb
 mkdir -p $SIM_RESULT_DIR
@@ -23,8 +44,7 @@ mkdir -p $VER_OBJ_DIR
 make testbench_verilator SIM_RESULTS=$SIM_RESULT_DIR SIM_TEST_RESULTS=$SIM_RESULT_DIR VERI_OBJ_DIR=$VERI_OBJ_DIR 
 
 # Build elf with input file
-make $SIM_RESULT_DIR/$1.hex -f $COMMON_MK TEST_TEST_DIR=$USER_PROG_DIR SIM_TEST_PROGRAM_RESULTS=$SIM_RESULT_DIR SIM_BSP_RESULTS=$SIM_RESULT_DIR/bsp
+make $SIM_RESULT_DIR/$USER_PROG_DIR.hex -f $COMMON_MK TEST_TEST_DIR=$USER_PROG_DIR SIM_TEST_PROGRAM_RESULTS=$SIM_RESULT_DIR SIM_BSP_RESULTS=$SIM_RESULT_DIR/bsp
 
 # Simulation
-touch $SIM_RESULT_DIR/$1_sim.log
-./$SIM_RESULT_DIR/verilator_executable $VERI_FLAGS "+firmware=$SIM_RESULT_DIR/$1.hex" | tee $SIM_RESULT_DIR/$1_sim.log
+./$SIM_RESULT_DIR/verilator_executable $VERI_FLAGS "+firmware=$SIM_RESULT_DIR/$USER_PROG_DIR.hex" | tee $SIM_RESULT_DIR/${USER_PROG_DIR}_sim.log
