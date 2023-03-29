2b8833ae3dc33676aebbffc089fcb6389b40b41b
Yutung <cheng056.ece05g@nctu.edu.tw>
2023-03-29 13:31:25 +0800

    build core-tb with Verilator and sim user prog

--- cv32e40p/sim/core/run_coretb.sh
+++ cv32e40p/sim/core/run_coretb.sh
@@ -14,7 +14,17 @@ fi
 export USER_PROG_DIR=$1
 export COMMON_MK=$CORE_V_VERIF/mk/Common.mk
 export SIM_RESULT_DIR=$1_sim_result
+export VERI_OBJ_DIR=$SIM_RESULT_DIR/cobj_dir
+
+
+# Build core-tb
+mkdir -p $SIM_RESULT_DIR
+mkdir -p $VER_OBJ_DIR
+make testbench_verilator SIM_RESULTS=$SIM_RESULT_DIR SIM_TEST_RESULTS=$SIM_RESULT_DIR VERI_OBJ_DIR=$VERI_OBJ_DIR 
 
 # Build elf with input file
-mkdir -p $SIM_RESULT_DIR
 make $SIM_RESULT_DIR/$1.hex -f $COMMON_MK TEST_TEST_DIR=$USER_PROG_DIR SIM_TEST_PROGRAM_RESULTS=$SIM_RESULT_DIR SIM_BSP_RESULTS=$SIM_RESULT_DIR/bsp
+
+# Simulation
+touch $SIM_RESULT_DIR/$1_sim.log
+./$SIM_RESULT_DIR/verilator_executable $VERI_FLAGS "+firmware=$SIM_RESULT_DIR/$1.hex" | tee $SIM_RESULT_DIR/$1_sim.log
